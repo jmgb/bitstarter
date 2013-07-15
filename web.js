29adfc4f9095ace1427d311c1a0edf51bbd6da0a
Ubuntu <ubuntu@ip-172-31-31-153.us-west-2.compute.internal>
2013-07-15 18:27:17 +0000

    cambio en console log

--- web.js
+++ web.js
@@ -1,4 +1,6 @@
-var express = require('express');
+#!/usr/bin/env node
+
+var express = require("express");
 
 var app = express.createServer(express.logger());
 
@@ -9,13 +11,14 @@ fs = require('fs');
 fs.readFile('index.html', function (err, data) { 
 if (err){return console.log(err);}
 buffer=data;
-console.log(data + "leido con exito"); });
+//console.log(data + "leido con exito"); 
+});
 
 app.get('/', function(request, response) {
   response.send(buffer.toString('utf-8'));
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
